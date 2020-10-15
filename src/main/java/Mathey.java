2563e5bcfa0e88e633e8801479e3da357a8e8de2
MetalTurtle <dekolis1218@gmail.com>
2020-10-15 11:31:50 -0700

    Do the exercises from class

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -1,3 +1,32 @@
 public class Mathey {
-
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+    public static double max(double a, double b) {
+        return Math.max(a, b);
+    }
+    public static int max(int a, int b, int c) {
+        return Math.max(Math.max(a, b), c);
+    }
+    public static double max(double a, double b, double c) {
+        return Math.max(Math.max(a, b), c);
+    }
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+    public static double min(double a, double b) {
+        return Math.min(a, b);
+    }
+    public static int min(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
+    public static double min(double a, double b, double c) {
+        return Math.min(Math.min(a, b), c);
+    }
+    public static int randomInteger(int a) {
+        return (int) (Math.random() * (a + 1));
+    }
+    public static int randomInteger(int a, int b) {
+        return (int) (Math.random() * (a + 1) + b);
+    }
 }
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class MyMain {
 
-    public static int someMethod() {
-        return 1;
+    public static void randomNumbers() {
+        int a = (int) (Math.random() * 11 + 10);
+        int b = (int) (Math.random() * 11 + 10);
+        int c = (int) (Math.random() * 11 + 10);
+        System.out.println("The three random numbers are " + a + ", " + b + ", and " + c);
+        System.out.println("The largest number is " + Math.max(Math.max(a, b), c));
+        System.out.println("The smallest number is " + Math.min(Math.min(a, b), c));
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        randomNumbers();
     }
 
 }
